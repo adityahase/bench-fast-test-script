1dee12beb1ba315dca621a6a9e4fdde93f30bf45
Aditya Hase <aditya@adityahase.com>
2017-07-28 17:15:04 +0530

    Show latest termbin link and preserve log.link

--- fast.sh
+++ fast.sh
@@ -10,4 +10,5 @@ git clone https://github.com/adityahase/bench --depth=1 -b develop bench-repo
 virtualenv -p python$2 $2 && source $2/bin/activate && pip install -e bench-repo
 # Run bench init with frappe repo (develop branch) and log output
 bench init b --verbose --frappe-path="https://github.com/adityahase/frappe" --frappe-branch=develop 2>&1 | tee log.log
-cat log.log | nc termbin.com 9999 | sed -e "s/^/$(date -R) /" >> log.link
+cat log.log | nc termbin.com 9999 | sed -e "s/^/$1 $(date -R) /" >> /home/frappe/log.link
+tail -1 log.link
